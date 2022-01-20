f018f6579bae28145509389cf16bed0114cdacb2
PraneshBalasubramaniam <44346322+PraneshBalasubramaniam@users.noreply.github.com>
2022-01-20 11:33:12 +0530

    formatted & small changes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
 import { useEffect, useState } from 'react';
-import Recipe from './Recipe';
+import Recipe from './components/Recipe';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = "36f361f5";
   const APP_KEY = "bbe5ba0a1e22ae5027a5b1aab2d0b71e";
-  
+
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("Chicken");
-  
-  const updateChange = e => {
+
+  const updateSearchChange = e => {
     setSearch(e.target.value);
-    console.log(search);
+    // console.log(search);
   }
-  
+
   const getSearch = e => {
     e.preventDefault();
-    console.log("Prevented Reload");
+    // console.log("Prevented Reload");
     setQuery(search);
-    console.log(search,query);
+    // console.log(search, query);
     setSearch('');
-    console.log(search);
+    // console.log(search);
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     getRecipes();
-    console.log("Lets see if it works");
+    // console.log("Lets see if it works");
   }, [query]);
 
-  const getRecipes = async () =>{
-  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-  const data = await response.json();
-  console.log(data.hits);
-  setRecipes(data.hits);
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    // console.log(data.hits);
+    setRecipes(data.hits);
   }
-  
+
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateChange} />
+        <input className="search-bar" type="text" value={search} onChange={updateSearchChange} />
         <button className="search-button" type="submit">Search</button>
       </form>
       <div className="recipes">
-      {recipes.map(recipe =>(
-        <Recipe
-        key={recipe.recipe.label}
-        title={recipe.recipe.label}
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}
-        />
-      ))}
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,23 +1,23 @@
 import React from "react";
 import style from "./recipe.module.css";
 
-const Recipe = ({title,calories,image,ingredients}) => {
-    return(
+const Recipe = ({ title, calories, image, ingredients }) => {
+    return (
         <div className={style.recipe} >
-{/* title */}
+            {/* title */}
             <h1>{title}</h1>
-{/* image */}
+            {/* image */}
             <img className={style.image} src={image} alt="" />
-{/* ingredients */}
+            {/* ingredients */}
             <ol>
                 <b>Ingredients</b>
                 <div className={style.left_align}>
-                    {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                ))}
+                    {ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                    ))}
                 </div>
             </ol>
-{/* calories */}
+            {/* calories */}
             <p><b>Calories</b> : {calories}</p>
 
         </div>
